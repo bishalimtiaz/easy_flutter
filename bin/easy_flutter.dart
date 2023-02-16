48f32d226cabf052e0c612ee702a5a0f42732622
Bishal Imtiaz <bishal.imtiaz@brainstation-23.com>
2023-02-16 18:40:02 +0600

    Initial commit

---
+++ bin/easy_flutter.dart
@@ -0,0 +1,5 @@
+import 'package:easy_flutter/easy_flutter.dart' as easy_flutter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${easy_flutter.calculate()}!');
+}
---
+++ test/easy_flutter_test.dart
@@ -0,0 +1,8 @@
+import 'package:easy_flutter/easy_flutter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
